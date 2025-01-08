d32291b89f7ef82053215592c5acb49d5a67566e
onthir <bibek09@outlook.com>
2025-01-08 16:58:51 -0600

    updated url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,7 @@
 
-const API_URL = "http://127.0.0.1:8000/api";
+
+
+const API_URL = process.env.REACT_APP_API_URL; 
 
 // function to recommend songs
 export const createRecommendation = async (query) => {
